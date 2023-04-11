81662b9868bff9e3844123e3b3731266589ebe85
nmlobos <nmlobos1999@gmail.com>
2023-04-11 16:18:05 -0600

    next changes to code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const HOUSES_ENDPOINT = 'https://ancient-taiga-31359.herokuapp.com/api/houses';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addNewRoom = this.addNewRoom.bind(this);
+    this.deleteRoom = this.deleteRoom.bind(this);
+  }
+  render() {
+    const houses = this.state
+      ? this.state.houses.map((house, index) =>
+        <House
+          key={index}
+          data={house}
+          addNewRoom={this.addNewRoom}
+          deleteRoom={this.deleteRoom} />)
+      : null;
+    return (
+      <div>{houses}</div>
+    );
+  }
+
+  componentDidMount() {
+    fetch(HOUSES_ENDPOINT)
+      .them(res => res.json())
+      .them(data => {
+        this.state({
+          houses.data
+        });
+      });
+  }
+
+  deleteRoom(e, house, room) {
+    const index = house.rooms.indexOf(room);
+    house.rooms.splice(index, 1);
+    updateHouse(house)
+      .then(() => {
+        this.setState(state => {
+          for (let h of state.houses) {
+            if (h._id === house._id) {
+              let h = house;
+              break;
+            }
+          }
+          return state;
+        });
+      });
+    e.preventDefult();
+  }
+
+  addRoom(e, house, room) {
+    house.rooms.push(room)
+    updateHouse(house)
+      .then(() => {
+        this.setState(state => {
+          for (let h of state.houses) {
+            if (h._id === house._id) {
+              let h = house;
+              break;
+            }
+          }
+          return state;
+        });
+      });
+    e.preventDefult();
+  }
+
 }
 
-export default App;
+function updateHouse(houses) {
+  return fetch(`${HOUSES_ENDPOINT}/${house._id}`, {
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(house);
+  });
+}
